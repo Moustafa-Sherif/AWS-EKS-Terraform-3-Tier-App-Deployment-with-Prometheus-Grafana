c31834e97652e85646d011fe457346799fc84c3b
moustafa.sherif <moustafa.sherif@widebot.net>
2024-02-28 14:23:54 +0200

    added .gitignore file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sandip-demo-tfstate-bucket"
+    bucket = "devops-demo-tfstate-bucket-us-west-2"
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 }
-
-
